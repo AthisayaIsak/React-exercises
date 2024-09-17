a0525cf98f8f44def167c01633a063af022643f2
AthisayaIsak <727723eucd008@skcet.ac.in>
2024-09-17 20:35:10 +0530

    Add files via upload

---
+++ day 3 class exercise.js
@@ -0,0 +1,681 @@
+Skip to content <#start-of-content>
+
+
+    Navigation Menu
+
+Toggle navigation
+<https://github.com/>
+Sign in <https://github.com/login?
+return_to=https%3A%2F%2Fgithub.com%2FIndhumathis26%2FReact-Exercises>
+
+  *
+    Product
+
+      o
+
+        Actions
+        Automate any workflow
+        <https://github.com/features/actions>
+      o
+
+        Packages
+        Host and manage packages
+        <https://github.com/features/packages>
+      o
+
+        Security
+        Find and fix vulnerabilities
+        <https://github.com/features/security>
+      o
+
+        Codespaces
+        Instant dev environments
+        <https://github.com/features/codespaces>
+      o
+
+        GitHub Copilot
+        Write better code with AI
+        <https://github.com/features/copilot>
+      o
+
+        Code review
+        Manage code changes
+        <https://github.com/features/code-review>
+      o
+
+        Issues
+        Plan and track work
+        <https://github.com/features/issues>
+      o
+
+        Discussions
+        Collaborate outside of code
+        <https://github.com/features/discussions>
+    Explore
+      o All features <https://github.com/features>
+      o Documentation
+
+        <https://docs.github.com/>
+      o GitHub Skills
+
+        <https://skills.github.com/>
+      o Blog
+
+        <https://github.blog/>
+  *
+    Solutions
+
+    By size
+      o Enterprise <https://github.com/enterprise>
+      o Teams <https://github.com/team>
+      o Startups <https://github.com/enterprise/startups>
+    By industry
+      o Healthcare <https://github.com/solutions/industries/healthcare>
+      o Financial services <https://github.com/solutions/industries/
+        financial-services>
+      o Manufacturing <https://github.com/solutions/industries/
+        manufacturing>
+    By use case
+      o CI/CD & Automation <https://github.com/solutions/ci-cd>
+      o DevOps <https://github.com/solutions/devops>
+      o DevSecOps <https://github.com/solutions/devsecops>
+  *
+    Resources
+
+    Topics
+      o AI <https://github.com/resources/articles/ai>
+      o DevOps <https://github.com/resources/articles/devops>
+      o Security <https://github.com/resources/articles/security>
+      o Software Development <https://github.com/resources/articles/
+        software-development>
+      o View all <https://github.com/resources/articles>
+    Explore
+      o Learning Pathways
+
+        <https://resources.github.com/learn/pathways>
+      o White papers, Ebooks, Webinars
+
+        <https://resources.github.com/>
+      o Customer Stories <https://github.com/customer-stories>
+      o Partners
+
+        <https://partner.github.com/>
+  *
+    Open Source
+
+      o
+        GitHub Sponsors
+        Fund open source developers
+        <https://github.com/sponsors>
+      o
+        The ReadME Project
+        GitHub community articles
+        <https://github.com/readme>
+    Repositories
+      o Topics <https://github.com/topics>
+      o Trending <https://github.com/trending>
+      o Collections <https://github.com/collections>
+  *
+    Enterprise
+
+      o
+
+        Enterprise platform
+        AI-powered developer platform
+        <https://github.com/enterprise>
+    Available add-ons
+      o
+
+        Advanced Security
+        Enterprise-grade security features
+        <https://github.com/enterprise/advanced-security>
+      o
+
+        GitHub Copilot
+        Enterprise-grade AI features
+        <https://github.com/features/copilot#enterprise>
+      o
+
+        Premium Support
+        Enterprise-grade 24/7 support
+        <https://github.com/premium-support>
+  * Pricing <https://github.com/pricing>
+
+Search or jump to...
+
+
+  Search code, repositories, users, issues, pull requests...
+
+Search
+
+Clear
+
+Search syntax tips <https://docs.github.com/search-github/github-code-
+search/understanding-github-code-search-syntax>
+
+
+  Provide feedback
+
+We read every piece of feedback, and take your input very seriously.
+
+Include my email address so I can be contacted
+
+Cancel
+
+Submit feedback
+
+
+  Saved searches
+
+
+    Use saved searches to filter your results more quickly
+
+Name
+Query
+
+To see all available qualifiers, see our documentation <https://
+docs.github.com/search-github/github-code-search/understanding-github-
+code-search-syntax>.
+
+Cancel
+
+Create saved search
+
+Sign in <https://github.com/login?
+return_to=https%3A%2F%2Fgithub.com%2FIndhumathis26%2FReact-Exercises>
+Sign up <https://github.com/signup?
+ref_cta=Sign+up&ref_loc=header+logged+out&ref_page=%2F%3Cuser-
+name%3E%2F%3Crepo-name%3E&source=header-
+repo&source_repo=Indhumathis26%2FReact-Exercises> Reseting focus
+
+You signed in with another tab or window. Reload to refresh your
+session. You signed out in another tab or window. Reload to refresh your
+session. You switched accounts on another tab or window. Reload to
+refresh your session.
+
+Dismiss alert
+{{ message }}
+
+Indhumathis26 <https://github.com/Indhumathis26> /
+*React-Exercises <https://github.com/Indhumathis26/React-Exercises>
+
+* Public
+
+  * Notifications <https://github.com/login?
+    return_to=%2FIndhumathis26%2FReact-Exercises> You must be signed in
+    to change notification settings
+  * Fork 0 <https://github.com/login?return_to=%2FIndhumathis26%2FReact-
+    Exercises>
+  *
+    Star 0 <https://github.com/login?return_to=%2FIndhumathis26%2FReact-
+    Exercises>
+
+0 stars <https://github.com/Indhumathis26/React-Exercises/stargazers> 0
+forks <https://github.com/Indhumathis26/React-Exercises/forks> Branches
+<https://github.com/Indhumathis26/React-Exercises/branches> Tags
+<https://github.com/Indhumathis26/React-Exercises/tags> Activity
+<https://github.com/Indhumathis26/React-Exercises/activity>
+Star <https://github.com/login?return_to=%2FIndhumathis26%2FReact-Exercises>
+Notifications <https://github.com/login?
+return_to=%2FIndhumathis26%2FReact-Exercises> You must be signed in to
+change notification settings
+
+  *
+
+    Code <https://github.com/Indhumathis26/React-Exercises>
+  *
+
+    Issues 0 <https://github.com/Indhumathis26/React-Exercises/issues>
+  *
+
+    Pull requests 0 <https://github.com/Indhumathis26/React-Exercises/pulls>
+  *
+
+    Actions <https://github.com/Indhumathis26/React-Exercises/actions>
+  *
+
+    Projects 0 <https://github.com/Indhumathis26/React-Exercises/projects>
+  *
+
+    Security <https://github.com/Indhumathis26/React-Exercises/security>
+  *
+
+    Insights <https://github.com/Indhumathis26/React-Exercises/pulse>
+
+Additional navigation options
+
+  *
+
+    Code <https://github.com/Indhumathis26/React-Exercises>
+  *
+
+    Issues <https://github.com/Indhumathis26/React-Exercises/issues>
+  *
+
+    Pull requests <https://github.com/Indhumathis26/React-Exercises/pulls>
+  *
+
+    Actions <https://github.com/Indhumathis26/React-Exercises/actions>
+  *
+
+    Projects <https://github.com/Indhumathis26/React-Exercises/projects>
+  *
+
+    Security <https://github.com/Indhumathis26/React-Exercises/security>
+  *
+
+    Insights <https://github.com/Indhumathis26/React-Exercises/pulse>
+
+
+  Indhumathis26/React-Exercises
+
+This commit does not belong to any branch on this repository, and may
+belong to a fork outside of the repository.
+ main
+
+*1* Branch
+<https://github.com/Indhumathis26/React-Exercises/branches>
+
+*0* Tags
+<https://github.com/Indhumathis26/React-Exercises/tags>
+
+<https://github.com/Indhumathis26/React-Exercises/branches>
+
+<https://github.com/Indhumathis26/React-Exercises/tags>
+Go to file
+
+Code
+
+
+    Folders and files
+
+Name	Name	Last commit message
+	Last commit date
+
+
+    Latest commit
+
+Indhumathis26 <https://github.com/Indhumathis26>Indhumathis26 <https://
+github.com/Indhumathis26/React-Exercises/commits?author=Indhumathis26>
+Create day5_pah.js <https://github.com/Indhumathis26/React-Exercises/
+commit/3e7dbd432bc2d7b29f0e4ca7d727b845927e5e50>
+Sep 3, 2024
+3e7dbd4 <https://github.com/Indhumathis26/React-Exercises/
+commit/3e7dbd432bc2d7b29f0e4ca7d727b845927e5e50> · 
+Sep 3, 2024
+
+
+    History
+
+25 Commits <https://github.com/Indhumathis26/React-Exercises/commits/main/>
+
+<https://github.com/Indhumathis26/React-Exercises/commits/main/>
+
+day-1(cw-1) <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day-1(cw-1)>
+	
+
+day-1(cw-1) <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day-1(cw-1)>
+	
+Create day-1(cw-1) <https://github.com/Indhumathis26/React-Exercises/
+commit/eb1f4b62568a661ba8d102b47de29708d5f5f907>
+	
+Sep 3, 2024
+
+day-1(cw-2) <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day-1(cw-2)>
+	
+
+day-1(cw-2) <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day-1(cw-2)>
+	
+Create day-1(cw-2) <https://github.com/Indhumathis26/React-Exercises/
+commit/76c806fc2ba2052880d3a93cdffc02ca33b237ce>
+	
+Sep 3, 2024
+
+day-1(cw-3) <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day-1(cw-3)>
+	
+
+day-1(cw-3) <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day-1(cw-3)>
+	
+Create day-1(cw-3) <https://github.com/Indhumathis26/React-Exercises/
+commit/c7fec4ea48944283a0e24fddd7a549a51770abe7>
+	
+Sep 3, 2024
+
+day-1(pah-1) <https://github.com/Indhumathis26/React-Exercises/blob/
+main/day-1(pah-1)>
+	
+
+day-1(pah-1) <https://github.com/Indhumathis26/React-Exercises/blob/
+main/day-1(pah-1)>
+	
+Create day-1(pah-1) <https://github.com/Indhumathis26/React-Exercises/
+commit/fd3fbb62d6f01fd023cd39a7cf76103eb6dc530c>
+	
+Sep 3, 2024
+
+day-1(pah-2) <https://github.com/Indhumathis26/React-Exercises/blob/
+main/day-1(pah-2)>
+	
+
+day-1(pah-2) <https://github.com/Indhumathis26/React-Exercises/blob/
+main/day-1(pah-2)>
+	
+Create day-1(pah-2) <https://github.com/Indhumathis26/React-Exercises/
+commit/a59f2cdf153e772e9ab822735401b6431c45e9ad>
+	
+Sep 3, 2024
+
+day-3 class exercise.js <https://github.com/Indhumathis26/React-
+Exercises/blob/main/day-3%20class%20exercise.js>
+	
+
+day-3 class exercise.js <https://github.com/Indhumathis26/React-
+Exercises/blob/main/day-3%20class%20exercise.js>
+	
+Create day-3 class exercise.js <https://github.com/Indhumathis26/React-
+Exercises/commit/0ad9a670a88552526cf4a318339b0d55bf8fa682>
+	
+Aug 16, 2024
+
+day1_cy1.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day1_cy1.js>
+	
+
+day1_cy1.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day1_cy1.js>
+	
+Create day1_cy1.js <https://github.com/Indhumathis26/React-Exercises/
+commit/f8ab2545522b28ab8ffba70f542444b730bb2b8b>
+	
+Sep 3, 2024
+
+day1_cy2.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day1_cy2.js>
+	
+
+day1_cy2.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day1_cy2.js>
+	
+Create day1_cy2.js <https://github.com/Indhumathis26/React-Exercises/
+commit/b44e0e9d89818b98e2d1e5dfd9f743035de1212f>
+	
+Sep 3, 2024
+
+day1_dc.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day1_dc.js>
+	
+
+day1_dc.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day1_dc.js>
+	
+Create day1_dc.js <https://github.com/Indhumathis26/React-Exercises/
+commit/39ed14ed13046b607ce9beb03ecc572c17b9499b>
+	
+Sep 3, 2024
+
+day2_c21.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day2_c21.js>
+	
+
+day2_c21.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day2_c21.js>
+	
+Create day2_c21.js <https://github.com/Indhumathis26/React-Exercises/
+commit/b1fe394e9a24f2aa8389e9438f73014b140e30d2>
+	
+Sep 3, 2024
+
+day2_ce2.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day2_ce2.js>
+	
+
+day2_ce2.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day2_ce2.js>
+	
+Create day2_ce2.js <https://github.com/Indhumathis26/React-Exercises/
+commit/17ec0e162b738b51c4ae2b80323f764ad8480152>
+	
+Sep 3, 2024
+
+day2_cy.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day2_cy.js>
+	
+
+day2_cy.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day2_cy.js>
+	
+Create day2_cy.js <https://github.com/Indhumathis26/React-Exercises/
+commit/9a7390ea78772245dce9c150f61c09282ef00964>
+	
+Sep 3, 2024
+
+day2_dc.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day2_dc.js>
+	
+
+day2_dc.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day2_dc.js>
+	
+Create day2_dc.js <https://github.com/Indhumathis26/React-Exercises/
+commit/5cceeca1c860f77c04cf04cc801cdc95b74cc32f>
+	
+Sep 3, 2024
+
+day2_pah.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day2_pah.js>
+	
+
+day2_pah.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day2_pah.js>
+	
+Create day2_pah.js <https://github.com/Indhumathis26/React-Exercises/
+commit/26c953f8f13961747ef26d3d4967228a7a9ea819>
+	
+Sep 3, 2024
+
+day3_cy.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day3_cy.js>
+	
+
+day3_cy.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day3_cy.js>
+	
+Create day3_cy.js <https://github.com/Indhumathis26/React-Exercises/
+commit/ae4db394c7eaa99c9d488ec6a29ccc5e3faa7e15>
+	
+Sep 3, 2024
+
+day3_dc.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day3_dc.js>
+	
+
+day3_dc.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day3_dc.js>
+	
+Create day3_dc.js <https://github.com/Indhumathis26/React-Exercises/
+commit/4bafdf74b3af5cdbf8651b2eea6e3d26d2286141>
+	
+Sep 3, 2024
+
+day3_pah.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day3_pah.js>
+	
+
+day3_pah.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day3_pah.js>
+	
+Create day3_pah.js <https://github.com/Indhumathis26/React-Exercises/
+commit/ed6101f5b69b7f2c8e43fceb28b5cd081c821067>
+	
+Sep 3, 2024
+
+day4_ce.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day4_ce.js>
+	
+
+day4_ce.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day4_ce.js>
+	
+Create day4_ce.js <https://github.com/Indhumathis26/React-Exercises/
+commit/c7b590fce7ebae70dc0ea3f92d10b71f3fe9f455>
+	
+Sep 3, 2024
+
+day4_cy.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day4_cy.js>
+	
+
+day4_cy.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day4_cy.js>
+	
+Create day4_cy.js <https://github.com/Indhumathis26/React-Exercises/
+commit/110d8d45e0e229748fa21394a219cdebca086d08>
+	
+Sep 3, 2024
+
+day4_dc.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day4_dc.js>
+	
+
+day4_dc.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day4_dc.js>
+	
+Create day4_dc.js <https://github.com/Indhumathis26/React-Exercises/
+commit/3a20261404f3d2b726c1e25f64cedc7864b14046>
+	
+Sep 3, 2024
+
+day4_pah.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day4_pah.js>
+	
+
+day4_pah.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day4_pah.js>
+	
+Create day4_pah.js <https://github.com/Indhumathis26/React-Exercises/
+commit/53f857f368471385f967db7765d540af73b2bef2>
+	
+Sep 3, 2024
+
+day5_ce.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day5_ce.js>
+	
+
+day5_ce.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day5_ce.js>
+	
+Create day5_ce.js <https://github.com/Indhumathis26/React-Exercises/
+commit/0cf17b85dd314477f80154b232a529f9f3105a22>
+	
+Sep 3, 2024
+
+day5_cy.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day5_cy.js>
+	
+
+day5_cy.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day5_cy.js>
+	
+Create day5_cy.js <https://github.com/Indhumathis26/React-Exercises/
+commit/b435cb5da56bc36d1aeb97692565c613021e242d>
+	
+Sep 3, 2024
+
+day5_dc.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day5_dc.js>
+	
+
+day5_dc.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day5_dc.js>
+	
+Create day5_dc.js <https://github.com/Indhumathis26/React-Exercises/
+commit/1652b0e0d56cdffabca415e9f46e2776e8502330>
+	
+Sep 3, 2024
+
+day5_pah.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day5_pah.js>
+	
+
+day5_pah.js <https://github.com/Indhumathis26/React-Exercises/blob/main/
+day5_pah.js>
+	
+Create day5_pah.js <https://github.com/Indhumathis26/React-Exercises/
+commit/3e7dbd432bc2d7b29f0e4ca7d727b845927e5e50>
+	
+Sep 3, 2024
+View all files
+
+
+    About
+
+No description, website, or topics provided.
+
+Activity <https://github.com/Indhumathis26/React-Exercises/activity>
+
+
+      Stars
+
+*0* stars <https://github.com/Indhumathis26/React-Exercises/stargazers>
+
+
+      Watchers
+
+*1* watching <https://github.com/Indhumathis26/React-Exercises/watchers>
+
+
+      Forks
+
+*0* forks <https://github.com/Indhumathis26/React-Exercises/forks>
+Report repository <https://github.com/contact/report-content?
+content_url=https%3A%2F%2Fgithub.com%2FIndhumathis26%2FReact-
+Exercises&report=Indhumathis26+%28user%29>
+
+
+    Releases <https://github.com/Indhumathis26/React-Exercises/releases>
+
+No releases published
+
+
+    Packages 0 <https://github.com/users/Indhumathis26/packages?
+    repo_name=React-Exercises>
+
+No packages published
+
+
+    Languages
+
+  *
+
+    JavaScript 100.0% <https://github.com/Indhumathis26/React-Exercises/
+    search?l=javascript>
+
+
+    Footer
+
+<https://github.com/> © 2024 GitHub, Inc.
+
+
+      Footer navigation
+
+  * Terms <https://docs.github.com/site-policy/github-terms/github-
+    terms-of-service>
+  * Privacy <https://docs.github.com/site-policy/privacy-policies/
+    github-privacy-statement>
+  * Security <https://github.com/security>
+  * Status <https://www.githubstatus.com/>
+  * Docs <https://docs.github.com/>
+  * Contact <https://support.github.com/?tags=dotcom-footer>
+  *
+    Manage cookies
+
+  *
+    Do not share my personal information
+
+You can’t perform that action at this time.
